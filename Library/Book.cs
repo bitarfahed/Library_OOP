3f9ef7e42ef671b2d6472b810ef8a68df48f2fe6
Fahed <bitarfahed@gmail.com>
2026-09-24 16:11:06 +0300

    basices of book class

--- Library/Book.cs
+++ Library/Book.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Library
 {
     internal class Book
     {
-        public static void hello()
-        {
-            Console.WriteLine("Hello, Fahed!");
+        private string _Name;
+        private string _Year_of_publish;
+        private string _Author;
+        private string _Type; 
 
+        public void ToString()
+        {
+            Console.WriteLine("Book's details: " + this._Name +
+                              "published at: " + this._Year_of_publish,
+                              "written by: " + this._Author,
+                              "it is about: " + this._Type);
         }
+        public Book(string name, string year, string author, string type)
+        {
+            this._Name = name;
+            this._Year_of_publish = year;
+            this._Author = author;
+            this._Type = type;
+        }
+
+
     }
 
 }
