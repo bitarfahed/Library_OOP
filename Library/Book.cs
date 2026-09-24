5c7c64c19e41a39d54e45a1963ed9cf59fce7164
Fahed <bitarfahed@gmail.com>
2026-09-24 22:56:17 +0300

    initial infrastructure of Book class

--- Library/Book.cs
+++ Library/Book.cs
@@ -5,28 +5,70 @@ using System.Text;
 
 namespace Library
 {
+    enum Type
+    {
+        fiction, non_fiction, kids, academic, reference
+    }
     internal class Book
     {
         private string _Name;
-        private string _Year_of_publish;
-        private string _Author;
-        private string _Type; 
+        private int _Year_of_publication;
+        private Author _Author;
+        private Type _Type;
 
         public void ToString()
         {
-            Console.WriteLine("Book's details: " + this._Name +
-                              "published at: " + this._Year_of_publish,
-                              "written by: " + this._Author,
+            Console.WriteLine("Book's details: \n" +
+                              "book's name: " + this._Name + " \n" + 
+                              "published at: " + this._Year_of_publication + "\n" +
+                              "written by: " + this._Author + " \n" +
                               "it is about: " + this._Type);
         }
-        public Book(string name, string year, string author, string type)
+        public Book(string name, int year, Author author, Type type)
         {
             this._Name = name;
-            this._Year_of_publish = year;
+            this._Year_of_publication = year;
             this._Author = author;
-            this._Type = type;
+            if(!Enum.IsDefined(typeof(Type), type)) throw new Exception("invalid value"); // maybe not needed
+            else this._Type = type;
         }
 
+        public string GetName()
+        {
+            return this._Name;
+        }
+        public int GetpublicationingYear()
+        {
+            return this._Year_of_publication;
+        }
+        public Author GetAuthor()
+        {
+            return this._Author;
+        }
+        public Enum GetType()
+        {
+            return this._Type;
+        }
+
+        public void SetName(string name)
+        {
+            this._Name = name;
+        }
+        public void SetYearOfPublication(int year)
+        {
+            DateTime now = DateTime.Now;
+            if (year> now.Year) throw new Exception("invalid value"); // maybe not needed
+            this._Year_of_publication = year;
+        }
+        public void SetAuthor(Author author)
+        {
+            this._Author = author;
+        }
+        public void SetType(Type type)
+        {
+            if (!Enum.IsDefined(typeof(Type), type)) throw new Exception("invalid value"); // maybe not needed
+            else this._Type = type;
+        }
 
     }
 
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, Fahed!");
+﻿using Library;
+
+Book b1 = new Book("a", "2025", "f", "fiction");
+b1.ToString();
