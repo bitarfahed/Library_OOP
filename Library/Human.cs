e4cf45f412bb380fc7307d189755f93346d38a03
Fahed <bitarfahed@gmail.com>
2026-09-26 12:50:07 +0300

    adding readonly field

--- Library/Human.cs
+++ Library/Human.cs
@@ -5,31 +5,48 @@ using System.Xml.Linq;
 
 namespace Library
 {
+    public enum Gender
+    {
+        Male, Female
+    }
     public class Human 
     {
         private string _Name;
         private int _BirthYear;
         private string _Nationality;
+        protected readonly Gender _Gender;
+        public static int _Human_counter=0;
 
         public override string ToString()
         {
             return "Name: " + this._Name + "\n" +
                    "Birth year: " + this._BirthYear + "\n" +
-                   "Nationality: " + this._Nationality;
+                   "Nationality: " + this._Nationality + "\n" +
+                   "Gender: " + this._Gender;
         }
-        protected Human(string name, int birthyear, string nationality)
+        protected Human(string name, int birthyear, string nationality, Gender gender)
         {
+            _Human_counter++;
             if (birthyear > DateTime.Now.Year) throw new ArgumentException("invalid birth-year. ", nameof(birthyear));
             if (name == null) throw new ArgumentException("name cannot be empty.", nameof(name));
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("name cannot be whitespaces", nameof(name));
             if (nationality == null) throw new ArgumentException("nationality cannot be empty.", nameof(nationality));
             if (string.IsNullOrWhiteSpace(nationality)) throw new ArgumentException("nationality cannot be whitespaces", nameof(nationality));
+            if (!Enum.IsDefined(typeof(Gender), gender)) throw new ArgumentException("Invalid gender.", nameof(gender));
 
             this._Name = name;
             this._BirthYear = birthyear;
             this._Nationality = nationality;
+            this._Gender = gender;
         }
 
+
+        public static int GetHumansCounter() 
+            /*Human.GetHumansCounter beacuse it is a class object.
+            without static it would be obj.GetHumansCounter and this is illegal because HumanCounter is defined as static */
+        {
+            return _Human_counter;
+        }
         protected string GetName()
         {
             return this._Name;
@@ -64,6 +81,10 @@ namespace Library
         protected int GetAge()
         {
             return DateTime.Now.Year - this._BirthYear;
+        }
+        protected Gender GetGender()
+        {
+            return this._Gender;
         }
     }
 }
